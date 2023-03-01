5c0684d066362b78955652a8ada0c606db5fc84b
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2023-03-01 16:48:09 +0300

    create any function

--- 14.ecart/src/components/Ecart.jsx
+++ 14.ecart/src/components/Ecart.jsx
@@ -7,9 +7,15 @@ const fruit = [
   {id: 2, name: 'Orange', price: 8, selected: 0, stock: 20},
 ]
 
+
 const Ecart = () => {
-  const [products, setProducts] = useState(fruit)
-  const [count, setCount] = useState(1)
+  const products = fruit
+  const [count, setCount] = useState()
+  const [carts, setCarts] = useState({})
+  const order = (item) => {
+    // const itemIndex = carts.findIndex(cart => cart.id === item.id);
+    setCarts({...carts,  item})
+  }
   const upDisbaled = () => {
     if (count >= 12) {
       return true
@@ -26,21 +32,28 @@ const Ecart = () => {
       <div className='pt-6 pb-4'>
         <h1 className='text-4xl'>Ecart</h1>
         <div>
-          <button disabled={upDisbaled()} className='border-2 rounded-lg p-2 border-green-400 mr-2 disabled:bg-blue-200'
-          onClick={() => setCount(count + 1)}>+1</button>
-          <button disabled={prevDisabled()} className='border-2 rounded-lg p-2 border-green-400 disabled:bg-blue-200'
-          onClick={() => setCount(count - 1)}>-1</button>
-          <span>Apple - $10 x {count}</span>
+          {
+            carts.length ? (
+              carts.map(cart => (
+                <div key={cart.id}>
+                    <button disabled={upDisbaled()} className='border-2 rounded-lg p-2 border-green-400 mr-2 disabled:bg-blue-200'>+1</button>
+                    <button disabled={prevDisabled()} className='border-2 rounded-lg p-2 border-green-400 disabled:bg-blue-200'
+                    onClick={() => setCount(count - 1)}>-1</button>
+                    <span>{cart.name} - ${cart.price} x {cart.selected}</span> 
+                </div>))) : (<p className='pt-4'>Your cart is empty</p>
+            )
+          }
         </div>
-        <p className='pt-4'>Your cart is empty</p>
       </div>
       <hr/>
       <div className='pt-4'>
         <h2 className='text-3xl'>Inventory</h2>
         {
           products.map(product => (
-            <div className='pt-4'>
-              <button className=' border-2 rounded-lg p-2 border-green-400'>Buy</button>
+            <div key={product.id} className='pt-4'>
+              <button className='border-2 rounded-lg p-2 border-green-400'
+              onClick={() => order(product.id)}
+              >Buy</button>
               {' '}{product.name} - {product.price}$ ({product.selected} selected, {product.stock} in stock)
             </div>
           ))
